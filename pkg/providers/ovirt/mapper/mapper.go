d04df18c4691dd1e1c92fd2d5b645369e9443f85
Ondra Machacek <omachace@redhat.com>
2020-03-31 13:05:49 +0200

    Map VM attributes
    This PR add mapping to all the core VM attributes.
    
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- pkg/providers/ovirt/mapper/mapper.go
+++ pkg/providers/ovirt/mapper/mapper.go
@@ -1,7 +1,9 @@
 package mapper
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 
 	v2vv1alpha1 "github.com/kubevirt/vm-import-operator/pkg/apis/v2v/v1alpha1"
 	provider "github.com/kubevirt/vm-import-operator/pkg/providers"
@@ -15,10 +17,22 @@ import (
 )
 
 const (
-	cdiAPIVersion = "cdi.kubevirt.io/v1alpha1"
+	cdiAPIVersion           = "cdi.kubevirt.io/v1alpha1"
+	customPropertyHugePages = "hugepages"
+	// LabelOrigin define name of the label which holds value of origin attribute of oVirt VM
+	LabelOrigin = "origin"
+	// LabelInstanceType define name of the label which holds value of instance type attribute of oVirt VM
+	LabelInstanceType = "instanceType"
+	// LabelTag define name of the label which holds tags of oVirt VM
+	LabelTag = "tags"
+	// AnnotationComment define name of the annotation which holds comment of oVirt VM
+	AnnotationComment = "comment"
+	// AnnotationSso define name of the annotation which holds sso method of oVirt VM
+	AnnotationSso = "sso"
 )
 
-var biosMapping = map[string]*kubevirtv1.Bootloader{
+// BiosTypeMapping defines mapping of BIOS types between oVirt and kubevirt domains
+var BiosTypeMapping = map[string]*kubevirtv1.Bootloader{
 	"q35_sea_bios":    &kubevirtv1.Bootloader{BIOS: &kubevirtv1.BIOS{}},
 	"q35_secure_boot": &kubevirtv1.Bootloader{BIOS: &kubevirtv1.BIOS{}},
 	"q35_ovmf":        &kubevirtv1.Bootloader{EFI: &kubevirtv1.EFI{}},
@@ -26,6 +40,7 @@ var biosMapping = map[string]*kubevirtv1.Bootloader{
 
 var archMapping = map[string]string{
 	"x86_64": "q35",
+	"ppc64":  "pseries",
 }
 
 // OvirtMapper is struct that holds attributes needed to map oVirt VM to kubevirt VM
@@ -69,6 +84,11 @@ func (o *OvirtMapper) MapVM(targetVMName *string) *kubevirtv1.VirtualMachine {
 		vmSpec.ObjectMeta.Name = vmName
 	}
 
+	// Map hostname
+	if fqdn, ok := o.vm.Fqdn(); ok {
+		vmSpec.Spec.Template.Spec.Hostname = fqdn
+	}
+
 	// Map CPU
 	vmSpec.Spec.Template.Spec.Domain.CPU = o.mapCPU()
 
@@ -78,12 +98,37 @@ func (o *OvirtMapper) MapVM(targetVMName *string) *kubevirtv1.VirtualMachine {
 	// Map machine type
 	vmSpec.Spec.Template.Spec.Domain.Machine = *o.mapArchitecture()
 
+	// High Availability is mapped to running field of VM spec
+	vmSpec.Spec.Running = o.mapHighAvailability()
+
+	// Memory set hugepages and guest memory
+	vmSpec.Spec.Template.Spec.Domain.Memory = o.mapMemory()
+
+	// Memory policy set the memory limit
+	vmSpec.Spec.Template.Spec.Domain.Resources = o.mapResourceRequirements()
+
+	// Devices
+	vmSpec.Spec.Template.Spec.Domain.Devices = *o.mapGraphicalConsoles()
+
+	// Map labels like origin, instance_type
+	vmSpec.ObjectMeta.Labels = o.mapLabels()
+
+	// Map annotations like sso
+	vmSpec.ObjectMeta.Annotations = o.mapAnnotations()
+
+	// Map placement policy
+	vmSpec.Spec.Template.Spec.EvictionStrategy = o.mapPlacementPolicy()
+
+	// Map timezone
+	vmSpec.Spec.Template.Spec.Domain.Clock = o.mapTimeZone()
+
 	return &vmSpec
 }
 
 // MapDisks map the oVirt VM disks to the map of CDI DataVolumes specification, where
 // map id is the id of the oVirt disk
 func (o *OvirtMapper) MapDisks() map[string]cdiv1.DataVolume {
+	// TODO: stateless, boot_devices, floppy/cdrom
 	diskAttachments, _ := o.vm.DiskAttachments()
 	dvs := make(map[string]cdiv1.DataVolume, len(diskAttachments.Slice()))
 
@@ -151,8 +196,7 @@ func (o *OvirtMapper) resolveVMName(targetVMName *string, vm *ovirtsdk.Vm) (stri
 }
 
 func (o *OvirtMapper) getStorageClassForDisk(disk *ovirtsdk.Disk, mappings *v2vv1alpha1.OvirtMappings) *string {
-	sds, _ := disk.StorageDomains()
-	sd := sds.Slice()[0]
+	sd, _ := disk.StorageDomain()
 	for _, mapping := range *mappings.StorageMappings {
 		if mapping.Source.Name != nil {
 			if name, _ := sd.Name(); name == *mapping.Source.Name {
@@ -173,25 +217,60 @@ func (o *OvirtMapper) getStorageClassForDisk(disk *ovirtsdk.Disk, mappings *v2vv
 
 func (o *OvirtMapper) mapArchitecture() *kubevirtv1.Machine {
 	arch, _ := o.vm.MustCpu().Architecture()
-	return &kubevirtv1.Machine{
+	machine := &kubevirtv1.Machine{
 		Type: archMapping[string(arch)],
 	}
+
+	// Override machine type in case custom emulated machine is specified
+	if custom, ok := o.vm.CustomEmulatedMachine(); ok {
+		machine.Type = custom
+	}
+
+	return machine
 }
 
 func (o *OvirtMapper) mapFirmware() *kubevirtv1.Firmware {
+	firmware := &kubevirtv1.Firmware{}
+
+	// Map bootloader
 	bios, _ := o.vm.Bios()
 	biosType, _ := bios.Type()
-	bootloader, ok := biosMapping[string(biosType)]
+	bootloader, ok := BiosTypeMapping[string(biosType)]
 	if ok {
 		bootloader = &kubevirtv1.Bootloader{BIOS: &kubevirtv1.BIOS{}}
 	}
-	return &kubevirtv1.Firmware{
-		Bootloader: bootloader,
+	firmware.Bootloader = bootloader
+
+	// Map serial number
+	serial := o.mapSerialNumber()
+	if serial != nil {
+		firmware.Serial = *serial
 	}
+
+	return firmware
+}
+
+func (o *OvirtMapper) mapSerialNumber() *string {
+	if serialNumber, ok := o.vm.SerialNumber(); ok {
+		var serial string
+		policy, _ := serialNumber.Policy()
+		if policy == ovirtsdk.SERIALNUMBERPOLICY_VM {
+			serial, _ = o.vm.Id()
+		} else if policy == ovirtsdk.SERIALNUMBERPOLICY_CUSTOM {
+			serial, _ = serialNumber.Value()
+		} else {
+			return nil
+		}
+		return &serial
+	}
+
+	return nil
 }
 
 func (o *OvirtMapper) mapCPU() *kubevirtv1.CPU {
 	cpu := kubevirtv1.CPU{}
+
+	// CPU topology
 	if cpuDef, available := o.vm.Cpu(); available {
 		if topology, available := cpuDef.Topology(); available {
 			if cores, available := topology.Cores(); available {
@@ -206,5 +285,153 @@ func (o *OvirtMapper) mapCPU() *kubevirtv1.CPU {
 		}
 	}
 
+	// Custom cpu model
+	if customCPU, available := o.vm.CustomCpuModel(); available {
+		cpu.Model = customCPU
+	}
+
 	return &cpu
+}
+
+func (o *OvirtMapper) mapHighAvailability() *bool {
+	enabled := false
+	ha, _ := o.vm.HighAvailability()
+	if enabled, _ := ha.Enabled(); enabled {
+		return &enabled
+	}
+
+	return &enabled
+}
+
+func (o *OvirtMapper) mapMemory() *kubevirtv1.Memory {
+	// Guest memory
+	ovirtVMMemory, _ := o.vm.Memory()
+	guestMemory, _ := resource.ParseQuantity(strconv.FormatInt(ovirtVMMemory, 10))
+	memory := &kubevirtv1.Memory{}
+	memory.Guest = &guestMemory
+
+	// HugePages
+	hp := o.mapHugePages()
+	if hp != nil {
+		memory.Hugepages = hp
+	}
+
+	return memory
+}
+
+func (o *OvirtMapper) mapHugePages() *kubevirtv1.Hugepages {
+	var hugePageSize *string
+	if customProperties, available := o.vm.CustomProperties(); available {
+		for _, cp := range customProperties.Slice() {
+			if cpName, _ := cp.Name(); cpName == customPropertyHugePages {
+				cmValue, _ := cp.Value()
+				hugePageSize = &cmValue
+				break
+			}
+		}
+	}
+	if hugePageSize != nil {
+		return &kubevirtv1.Hugepages{
+			// HugePage size in oVirt custom_property is in MiB, so we add Mi suffix here.
+			PageSize: fmt.Sprintf("%sMi", *hugePageSize),
+		}
+	}
+
+	return nil
+}
+
+func (o *OvirtMapper) mapResourceRequirements() kubevirtv1.ResourceRequirements {
+	reqs := kubevirtv1.ResourceRequirements{}
+	memoryPolicy, _ := o.vm.MemoryPolicy()
+	maxMemory, _ := memoryPolicy.Max()
+	maxMemoryQuantity, _ := resource.ParseQuantity(strconv.FormatInt(maxMemory, 10))
+	reqs.Limits = map[corev1.ResourceName]resource.Quantity{
+		corev1.ResourceMemory: maxMemoryQuantity,
+	}
+
+	return reqs
+}
+
+// Graphical console is attached only in case the VM is not in headless mode
+func (o *OvirtMapper) mapGraphicalConsoles() *kubevirtv1.Devices {
+	// GraphicsConsole
+	vncEnabled := true
+	if gc, _ := o.vm.GraphicsConsoles(); len(gc.Slice()) == 0 {
+		vncEnabled = false
+	}
+	devices := &kubevirtv1.Devices{}
+	devices.AutoattachGraphicsDevice = &vncEnabled
+
+	// SerialConsole
+	if console, ok := o.vm.Console(); ok {
+		consoleEnabled, _ := console.Enabled()
+		devices.AutoattachSerialConsole = &consoleEnabled
+	}
+
+	return devices
+}
+
+func (o *OvirtMapper) mapLabels() map[string]string {
+	labels := map[string]string{}
+
+	// Origin
+	labels[LabelOrigin], _ = o.vm.Origin()
+
+	// Instance type
+	if instanceType, ok := o.vm.InstanceType(); ok {
+		labels[LabelInstanceType], _ = instanceType.Name()
+	}
+
+	// Tags
+	if tags, ok := o.vm.Tags(); ok {
+		var tagList []string
+		for _, tag := range tags.Slice() {
+			tagName, _ := tag.Name()
+			tagList = append(tagList, tagName)
+		}
+		labels[LabelTag] = strings.Join(tagList, ",")
+	}
+
+	return labels
+}
+
+func (o *OvirtMapper) mapAnnotations() map[string]string {
+	annotations := map[string]string{}
+
+	// Comment
+	if comment, ok := o.vm.Comment(); ok {
+		annotations[AnnotationComment] = comment
+	}
+
+	// SSO
+	if sso, ok := o.vm.Sso(); ok {
+		if methods, _ := sso.Methods(); len(methods.Slice()) > 0 {
+			annotations[AnnotationSso] = string(ovirtsdk.SSOMETHOD_GUEST_AGENT)
+		} else {
+			annotations[AnnotationSso] = "disabled"
+		}
+	}
+
+	return annotations
+}
+
+func (o *OvirtMapper) mapPlacementPolicy() *kubevirtv1.EvictionStrategy {
+	placementPolicy, _ := o.vm.PlacementPolicy()
+	affinity, _ := placementPolicy.Affinity()
+	if affinity == ovirtsdk.VMAFFINITY_MIGRATABLE {
+		strategy := kubevirtv1.EvictionStrategyLiveMigrate
+		return &strategy
+	}
+
+	return nil
+}
+
+func (o *OvirtMapper) mapTimeZone() *kubevirtv1.Clock {
+	clock := &kubevirtv1.Clock{}
+	if timezone, ok := o.vm.TimeZone(); ok {
+		timezoneName, _ := timezone.Name()
+		clockOffsetTimezone := kubevirtv1.ClockOffsetTimezone(timezoneName)
+		clock.Timezone = &clockOffsetTimezone
+	}
+	return clock
 }
--- pkg/providers/ovirt/mapper/mapper_test.go
+++ pkg/providers/ovirt/mapper/mapper_test.go
@@ -1,6 +1,8 @@
 package mapper_test
 
 import (
+	"fmt"
+
 	provider "github.com/kubevirt/vm-import-operator/pkg/providers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,6 +11,8 @@ import (
 
 	"github.com/kubevirt/vm-import-operator/pkg/providers/ovirt/mapper"
 )
+
+const memoryGI = 1024 * 1024 * 1024
 
 var targetVMName = "myvm"
 
@@ -24,11 +28,11 @@ var _ = Describe("Test mapping virtual machine attributes", func() {
 		vmSpec = mapper.MapVM(&targetVMName)
 	})
 
-	It("For name", func() {
+	It("should map name", func() {
 		Expect(vmSpec.Name).To(Equal(vm.MustName()))
 	})
 
-	It("For CPU topology", func() {
+	It("should map CPU topology", func() {
 		vmTopology := vm.MustCpu().MustTopology()
 		vmSpecCPU := vmSpec.Spec.Template.Spec.Domain.CPU
 		Expect(vmSpecCPU.Cores).To(Equal(uint32(vmTopology.MustCores())))
@@ -36,9 +40,56 @@ var _ = Describe("Test mapping virtual machine attributes", func() {
 		Expect(vmSpecCPU.Threads).To(Equal(uint32(vmTopology.MustThreads())))
 	})
 
-	It("For BIOS", func() {
+	It("should map BIOS", func() {
 		bootloader := vmSpec.Spec.Template.Spec.Domain.Firmware.Bootloader.BIOS
 		Expect(bootloader).ToNot(BeNil())
+	})
+
+	It("should map HA", func() {
+		Expect(*vmSpec.Spec.Running).To(Equal(vm.MustHighAvailability().MustEnabled()))
+	})
+
+	It("should map Memory", func() {
+		// guest memory
+		guestMemory, _ := vmSpec.Spec.Template.Spec.Domain.Memory.Guest.AsInt64()
+		Expect(guestMemory).To(Equal(vm.MustMemory()))
+
+		// huge page
+		hugePageSize := vmSpec.Spec.Template.Spec.Domain.Memory.Hugepages.PageSize
+		mappedHugePageSize := vm.MustCustomProperties().Slice()[0].MustValue()
+		Expect(hugePageSize).To(Equal(fmt.Sprintf("%sMi", mappedHugePageSize)))
+
+		// max memory
+		memLimit, _ := vmSpec.Spec.Template.Spec.Domain.Resources.Limits.Memory().AsInt64()
+		memMax := vm.MustMemoryPolicy().MustMax()
+		Expect(memLimit).To(Equal(memMax))
+	})
+
+	It("should map graphics consoles", func() {
+		graphicsEnabled := vmSpec.Spec.Template.Spec.Domain.Devices.AutoattachGraphicsDevice
+		Expect(*graphicsEnabled).To(Equal(len(vm.MustGraphicsConsoles().Slice()) > 0))
+	})
+
+	It("should map timezone", func() {
+		timezone := vmSpec.Spec.Template.Spec.Domain.Clock.Timezone
+		Expect(string(*timezone)).To(Equal(vm.MustTimeZone().MustName()))
+	})
+
+	It("should map placement policy", func() {
+		evictionStrategy := vmSpec.Spec.Template.Spec.EvictionStrategy
+		Expect(string(*evictionStrategy)).ToNot(BeNil())
+	})
+
+	It("should map annotations", func() {
+		annotations := vmSpec.ObjectMeta.Annotations
+		Expect(annotations[mapper.AnnotationComment]).To(Equal(vm.MustComment()))
+		Expect(annotations[mapper.AnnotationSso]).To(Equal(string(vm.MustSso().MustMethods().Slice()[0].MustId())))
+	})
+
+	It("should map labels", func() {
+		labels := vmSpec.ObjectMeta.Labels
+		Expect(labels[mapper.LabelOrigin]).To(Equal(vm.MustOrigin()))
+		Expect(labels[mapper.LabelInstanceType]).To(Equal(vm.MustInstanceType().MustName()))
 	})
 })
 
@@ -55,6 +106,42 @@ func createVM() *ovirtsdk.Vm {
 						Cores(1).
 						Sockets(2).
 						Threads(4).
+						MustBuild()).
+				MustBuild()).
+		HighAvailability(
+			ovirtsdk.NewHighAvailabilityBuilder().
+				Enabled(true).
+				MustBuild()).
+		Memory(memoryGI).
+		MemoryPolicy(
+			ovirtsdk.NewMemoryPolicyBuilder().
+				Max(memoryGI).MustBuild()).
+		GraphicsConsolesOfAny(
+			ovirtsdk.NewGraphicsConsoleBuilder().
+				Name("testConsole").MustBuild()).
+		TimeZone(
+			ovirtsdk.NewTimeZoneBuilder().
+				Name(">Etc/GMT").MustBuild()).
+		PlacementPolicy(
+			ovirtsdk.NewVmPlacementPolicyBuilder().
+				Affinity(ovirtsdk.VMAFFINITY_MIGRATABLE).MustBuild()).
+		Origin("ovirt").
+		Comment("vmcomment").
+		InstanceType(
+			ovirtsdk.NewInstanceTypeBuilder().
+				Name("server").MustBuild()).
+		TagsOfAny(
+			ovirtsdk.NewTagBuilder().
+				Name("mytag").MustBuild()).
+		CustomPropertiesOfAny(
+			ovirtsdk.NewCustomPropertyBuilder().
+				Name("hugepages").
+				Value("2048").MustBuild()).
+		Sso(
+			ovirtsdk.NewSsoBuilder().
+				MethodsOfAny(
+					ovirtsdk.NewMethodBuilder().
+						Id(ovirtsdk.SSOMETHOD_GUEST_AGENT).
 						MustBuild()).
 				MustBuild()).
 		MustBuild()
