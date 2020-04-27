85f0a4a85cc0268bf151224279040a48d882ccef
Ondra Machacek <omachace@redhat.com>
2020-04-27 09:04:07 +0200

    Use ocp-4.3
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- cluster-sync/sync.sh
+++ cluster-sync/sync.sh
@@ -6,9 +6,6 @@ source ./cluster-up/hack/common.sh
 source ./cluster-up/cluster/${KUBEVIRT_PROVIDER}/provider.sh
 source ./cluster-sync/${KUBEVIRT_PROVIDER}/provider.sh
 
-# Set controller verbosity to 3 for functional tests.
-export VERBOSITY=3
-
 # Install CDI
 install_cdi
 
@@ -28,18 +25,10 @@ if [ "${KUBEVIRT_PROVIDER}" != "external" ]; then
   _kubectl wait deploy/imageio-deployment -n cdi --for=condition=Available --timeout=480s
 fi
 
-# Setup vm-import-opreator
-export RELEASE_VERSION=v0.15.2
-export GO111MODULE=on
+# TODO: fetch it dynamically
+export OPERATOR_VERSION=0.0.1
 
-set -x
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-operator-sdk --verbose build quay.io/kubevirt/vm-import-opreator
-
-_kubectl apply -f ./deploy/crds/v2v_v1alpha1_resourcemapping_crd.yaml
-_kubectl apply -f ./deploy/crds/v2v_v1alpha1_virtualmachineimport_crd.yaml
-_kubectl apply -f ./deploy/service_account.yaml
-_kubectl apply -f ./deploy/role.yaml
-_kubectl apply -f ./deploy/role_binding.yaml
-_kubectl apply -f ./deploy/operator.yaml
+_kubectl apply -f manifests/vm-import-operator/${OPERATOR_VERSION}/v2v_v1alpha1_resourcemapping_crd.yaml
+_kubectl apply -f manifests/vm-import-operator/${OPERATOR_VERSION}/v2v_v1alpha1_virtualmachineimport_crd.yaml
+_kubectl apply -f manifests/vm-import-operator/${OPERATOR_VERSION}/config_map.yaml
+_kubectl apply -f manifests/vm-import-operator/${OPERATOR_VERSION}/operator.yaml
