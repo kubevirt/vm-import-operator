cabec5f8e7353a860f8608c04cab1becc9dba9b6
Jakub Dzon <jdzon@redhat.com>
2020-04-30 13:13:53 +0200

    Networked VM e2e tests
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

---
+++ tests/basic_net_vm_import_test.go
@@ -0,0 +1,101 @@
+package tests_test
+
+import (
+	v2vv1alpha1 "github.com/kubevirt/vm-import-operator/pkg/apis/v2v/v1alpha1"
+	"github.com/kubevirt/vm-import-operator/tests/framework"
+	. "github.com/kubevirt/vm-import-operator/tests/matchers"
+	"github.com/kubevirt/vm-import-operator/tests/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "kubevirt.io/client-go/api/v1"
+)
+
+var (
+	podType   = "pod"
+	networkID = "123"
+)
+
+type networkedVmImportTest struct{}
+
+var _ = Describe("Networked VM import ", func() {
+	var (
+		f         = framework.NewFrameworkOrDie("networked-vm-import")
+		secret    corev1.Secret
+		namespace string
+		test      = networkedVmImportTest{}
+	)
+
+	BeforeEach(func() {
+		namespace = f.Namespace.Name
+		s, err := f.CreateOvirtSecretFromBlueprint()
+		if err != nil {
+			Fail("Cannot create secret: " + err.Error())
+		}
+		secret = s
+	})
+
+	It("should create started VM", func() {
+		vmi := utils.VirtualMachineImportCr("basic-network", namespace, secret.Name, f.NsPrefix)
+		vmi.Spec.StartVM = &trueVar
+		vmi.Spec.Source.Ovirt.Mappings = &v2vv1alpha1.OvirtMappings{
+			NetworkMappings: &[]v2vv1alpha1.ResourceMappingItem{
+				{Source: v2vv1alpha1.Source{ID: &networkID}, Type: &podType},
+			},
+		}
+
+		created, err := f.VMImportClient.V2vV1alpha1().VirtualMachineImports(namespace).Create(&vmi)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(created).To(BeSuccessful(f))
+
+		retrieved, _ := f.VMImportClient.V2vV1alpha1().VirtualMachineImports(namespace).Get(created.Name, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		vmBlueprint := v1.VirtualMachine{ObjectMeta: metav1.ObjectMeta{Name: retrieved.Status.TargetVMName, Namespace: namespace}}
+		Expect(vmBlueprint).To(BeRunning(f))
+
+		vm := test.validateTargetConfiguration(f, vmBlueprint.Name, vmBlueprint.Namespace)
+		Expect(vm.Spec.Template.Spec.Volumes[0].DataVolume.Name).To(HaveDefaultStorageClass(f))
+	})
+})
+
+func (t *networkedVmImportTest) validateTargetConfiguration(f *framework.Framework, vmName string, vmNamespace string) *v1.VirtualMachine {
+	vm, _ := f.KubeVirtClient.VirtualMachine(vmNamespace).Get(vmName, &metav1.GetOptions{})
+	spec := vm.Spec.Template.Spec
+
+	By("having correct machine type")
+	Expect(spec.Domain.Machine.Type).To(BeEquivalentTo("q35"))
+
+	By("having BIOS")
+	Expect(spec.Domain.Firmware.Bootloader.BIOS).NotTo(BeNil())
+	Expect(spec.Domain.Firmware.Bootloader.EFI).To(BeNil())
+
+	By("having correct CPU configuration")
+	cpu := spec.Domain.CPU
+	Expect(cpu.Cores).To(BeEquivalentTo(1))
+	Expect(cpu.Sockets).To(BeEquivalentTo(1))
+	Expect(cpu.Threads).To(BeEquivalentTo(1))
+
+	By("having correct network configuration")
+	Expect(spec.Networks).To(HaveLen(1))
+	Expect(spec.Networks[0].Pod).ToNot(BeNil())
+	Expect(spec.Domain.Devices.Interfaces).To(HaveLen(1))
+	Expect(spec.Domain.Devices.Interfaces[0].Name).To(BeEquivalentTo(spec.Networks[0].Name))
+
+	By("having correct clock settings")
+	Expect(spec.Domain.Clock.UTC).ToNot(BeNil())
+
+	By("having correct disk setup")
+	disks := spec.Domain.Devices.Disks
+	Expect(disks).To(HaveLen(1))
+	disk1 := disks[0]
+	Expect(disk1.Disk.Bus).To(BeEquivalentTo("virtio"))
+	Expect(*disk1.BootOrder).To(BeEquivalentTo(1))
+
+	By("having correct volumes")
+	Expect(spec.Volumes).To(HaveLen(1))
+
+	return vm
+}
