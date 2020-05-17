6583b0b56024d3fc2f8a13ca7bcad7e1dfd5fbe0
Moti Asayag <masayag@redhat.com>
2020-05-17 15:37:29 +0300

    Rename CONTAINER_TAG to IMAGE_TAG
    When generating the manifests, the value set for the image version is
    actually the IMAGE_TAG.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -4,7 +4,7 @@ set -e
 PROJECT_ROOT="$(readlink -e $(dirname "$BASH_SOURCE[0]")/..)"
 DEPLOY_DIR="${DEPLOY_DIR:-${PROJECT_ROOT}/manifests}"
 CONTAINER_PREFIX="${CONTAINER_PREFIX:-quay.io/kubevirt}"
-CONTAINER_TAG="${CONTAINER_TAG:-latest}"
+IMAGE_TAG="${IMAGE_TAG:-latest}"
 IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-Always}"
 KUBEVIRT_NAMESPACE="${REPLACE_KUBEVIRT_NAMESPACE:-kubevirt-hyperconverged}"
 OPERATOR_IMAGE="${OPERATOR_IMAGE:-vm-import-operator}"
@@ -24,7 +24,7 @@ for template in $templates; do
 
 	sed -e "s/{{NAMESPACE}}/$KUBEVIRT_NAMESPACE/g" \
 	    -e "s#{{CONTAINER_PREFIX}}#$CONTAINER_PREFIX#g" \
-		-e "s/{{CONTAINER_TAG}}/$CONTAINER_TAG/g" \
+		-e "s/{{IMAGE_TAG}}/$IMAGE_TAG/g" \
 		-e "s/{{IMAGE_PULL_POLICY}}/$IMAGE_PULL_POLICY/g" \
         -e "s/{{OPERATOR_IMAGE}}/$OPERATOR_IMAGE/g" \
 	$infile > $file
@@ -37,8 +37,8 @@ rendered=$( \
 	--csv-version=${CSV_VERSION} \
 	--replaces-csv-version=${CSV_VERSION_REPLACES} \
 	--namespace=${KUBEVIRT_NAMESPACE} \
-	--operator-version=${CONTAINER_TAG} \
-	--operator-image="${CONTAINER_PREFIX}/${OPERATOR_IMAGE}" \
+	--operator-version=${IMAGE_TAG} \
+	--operator-image="${CONTAINER_PREFIX}/${OPERATOR_IMAGE}:${IMAGE_TAG}" \
 	--image-pull-policy=${IMAGE_PULL_POLICY} \
 )
 if [[ ! -z "$rendered" ]]; then
