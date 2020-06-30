f047658584ecd9e063e002853aff3b7de87c046f
Sam Lucidi <slucidi@redhat.com>
2020-06-30 15:32:06 -0400

    Set a timeout for Vmware api requests
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/providers/vmware/client/client.go
+++ pkg/providers/vmware/client/client.go
@@ -6,7 +6,11 @@ import (
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/vim25/types"
 	"net/url"
+	"time"
 )
+
+// timeout value in seconds for vmware api requests
+const timeout = 5 * time.Second
 
 // RichOvirtClient is responsible for retrieving VM data from oVirt API
 type richVmwareClient struct {
@@ -15,6 +19,9 @@ type richVmwareClient struct {
 
 // NewRichVMwareClient creates new, connected rich vmware client. After it is no longer needed, call Close().
 func NewRichVMWareClient(apiUrl, username, password string, insecure bool) (*richVmwareClient, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
 	u, err := url.Parse(apiUrl)
 	if err != nil {
 		return nil, err
@@ -22,7 +29,8 @@ func NewRichVMWareClient(apiUrl, username, password string, insecure bool) (*ric
 	if u.User == nil {
 		u.User = url.UserPassword(username, password)
 	}
-	govmomiClient, err := govmomi.NewClient(context.TODO(), u, insecure)
+
+	govmomiClient, err := govmomi.NewClient(ctx, u, insecure)
 	if err != nil {
 		return nil, err
 	}
@@ -43,39 +51,45 @@ func (r richVmwareClient) getVM(id string) (*object.VirtualMachine, error) {
 }
 
 func (r richVmwareClient) StopVM(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
 	vm, err := r.getVM(id)
 	if err != nil {
 		return err
 	}
-	powerState, err := vm.PowerState(context.TODO())
+	powerState, err := vm.PowerState(ctx)
 	if err != nil {
 		return err
 	}
 	if powerState != types.VirtualMachinePowerStatePoweredOff {
-		task, err := vm.PowerOff(context.TODO())
+		task, err := vm.PowerOff(ctx)
 		if err != nil {
 			return err
 		}
-		return task.Wait(context.TODO())
+		return task.Wait(ctx)
 	}
 	return nil
 }
 
 func (r richVmwareClient) StartVM(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
 	vm, err := r.getVM(id)
 	if err != nil {
 		return err
 	}
-	powerState, err := vm.PowerState(context.TODO())
+	powerState, err := vm.PowerState(ctx)
 	if err != nil {
 		return err
 	}
 	if powerState != types.VirtualMachinePowerStatePoweredOn {
-		task, err := vm.PowerOn(context.TODO())
+		task, err := vm.PowerOn(ctx)
 		if err != nil {
 			return err
 		}
-		return task.Wait(context.TODO())
+		return task.Wait(ctx)
 	}
 	return nil
 }
