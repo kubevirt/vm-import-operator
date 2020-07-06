76a799687fbe085ba9217b66288c03e60434cc11
Sam Lucidi <slucidi@redhat.com>
2020-07-06 15:42:30 -0400

    Refactor shared *Manager interfaces into provider package
    SecretsManager, ConfigMapsManager, DataVolumesManager, and
    VirtualMachineManager can all be used by the Vmware provider.
    This pulls those interfaces out of the oVirt package
    and into the root provider package.
    
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/providers/provider.go
+++ pkg/providers/provider.go
@@ -7,6 +7,7 @@ import (
 	kubevirtv1 "kubevirt.io/client-go/api/v1"
 	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
 	rclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -46,3 +47,29 @@ type Mapper interface {
 
 // VMStatus represents VM status
 type VMStatus string
+
+// SecretsManager defines operations on secrets
+type SecretsManager interface {
+	FindFor(types.NamespacedName) (*corev1.Secret, error)
+	CreateFor(*corev1.Secret, types.NamespacedName) error
+	DeleteFor(types.NamespacedName) error
+}
+
+// ConfigMapsManager defines operations on config maps
+type ConfigMapsManager interface {
+	FindFor(types.NamespacedName) (*corev1.ConfigMap, error)
+	CreateFor(*corev1.ConfigMap, types.NamespacedName) error
+	DeleteFor(types.NamespacedName) error
+}
+
+// DataVolumesManager defines operations on datavolumes
+type DataVolumesManager interface {
+	FindFor(types.NamespacedName) ([]*cdiv1.DataVolume, error)
+	DeleteFor(types.NamespacedName) error
+}
+
+// VirtualMachineManager defines operations on datavolumes
+type VirtualMachineManager interface {
+	FindFor(types.NamespacedName) (*kubevirtv1.VirtualMachine, error)
+	DeleteFor(types.NamespacedName) error
+}
