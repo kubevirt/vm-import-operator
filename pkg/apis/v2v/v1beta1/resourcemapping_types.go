e97a1ae0d2b1e4fdfb8a585656b12df02f2267d7
Sam Lucidi <slucidi@redhat.com>
2020-07-06 15:39:55 -0400

    Add Vmware import source and resource mapping specs
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/apis/v2v/v1beta1/resourcemapping_types.go
+++ pkg/apis/v2v/v1beta1/resourcemapping_types.go
@@ -17,6 +17,8 @@ type ResourceMappingSpec struct {
 
 	// +optional
 	OvirtMappings *OvirtMappings `json:"ovirt,omitempty"`
+	// +optional
+	VmwareMappings *VmwareMappings `json:"vmware,omitempty"`
 }
 
 // OvirtMappings defines the mappings of ovirt resources to kubevirt
@@ -39,6 +41,26 @@ type OvirtMappings struct {
 	// DiskMappings.Source.Name represents the disk alias on ovirt
 	// DiskMappings is respected only when provided in context of a single VM import within VirtualMachineImport
 	// +optional
+	DiskMappings *[]StorageResourceMappingItem `json:"diskMappings,omitempty"`
+}
+
+// VmwareMappings defines the mappings of vmware resources to kubevirt
+// +k8s:openapi-gen=true
+type VmwareMappings struct {
+	// NetworkMappings defines the mapping of guest network interfaces to kubevirt networks
+	// NetworkMappings.Source.Name represents the `network` field of the GuestNicInfo in vCenter
+	// NetworkMappings.Source.ID represents the `macAddress` field of the network adapter
+	// +optional
+	NetworkMappings *[]NetworkResourceMappingItem `json:"networkMappings,omitempty"`
+
+	// StorageMappings defines the mapping of datastores to storage classes
+	// +optional
+	StorageMappings *[]StorageResourceMappingItem `json:"storageMappings,omitempty"`
+
+	// +optional
+	// DiskMappings defines the mapping of VirtualDisks to storage classes
+	// DiskMappings.Source.Name represents the disk name in vCenter
+	// DiskMappings.Source.ID represents the `DiskObjectId` or `vDiskID` of the VirtualDisk in vCenter
 	DiskMappings *[]StorageResourceMappingItem `json:"diskMappings,omitempty"`
 }
 
