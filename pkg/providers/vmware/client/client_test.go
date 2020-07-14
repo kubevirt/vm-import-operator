98bb2e27a91228860830239a6acff470c6a0ed55
Sam Lucidi <slucidi@redhat.com>
2020-07-14 14:59:56 -0400

    Use table driven tests with parameters for vCenter and ESXi
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/providers/vmware/client/client_test.go
+++ pkg/providers/vmware/client/client_test.go
@@ -3,153 +3,137 @@ package client_test
 import (
 	"github.com/kubevirt/vm-import-operator/pkg/providers/vmware/client"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/simulator"
 )
 
-var _ = Describe("Test VMware rich client with vCenter", func() {
-	var (
-		model               *simulator.Model
-		server              *simulator.Server
-		vm                  *simulator.VirtualMachine
-		username            string
-		password            string
-		simulatorThumbprint = ""
+var _ = Describe("Test VMware rich client", func() {
+
+	DescribeTable("should connect to the host", func(model *simulator.Model) {
+		_ = model.Create()
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+
+		_, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
+		Expect(err).To(BeNil())
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
 	)
 
-	BeforeEach(func() {
-		model = simulator.VPX()
+	DescribeTable("should retrieve a VM by ID", func(model *simulator.Model) {
 		_ = model.Create()
-		server = model.Service.NewServer()
-		vm = simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
-		username = server.URL.User.Username()
-		password, _ = server.URL.User.Password()
-	})
-
-	AfterEach(func() {
-		model.Remove()
-		server.Close()
-	})
-
-	It("should connect to vCenter", func() {
-		_, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
-		Expect(err).To(BeNil())
-	})
-
-	It("should retrieve a VM by ID", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		vm := simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
 		Expect(err).To(BeNil())
 		vmRef := vm.Reference().Value
+
 		rawVm, err := richClient.GetVM(&vmRef, nil, nil, nil)
+
 		Expect(err).To(BeNil())
 		retrievedVm, ok := rawVm.(*object.VirtualMachine)
 		Expect(ok).To(BeTrue())
 		Expect(retrievedVm.Reference()).To(Equal(vm.Reference()))
-	})
-
-	It("should power off and on a VM by ID", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
-		Expect(err).To(BeNil())
-		vmRef := vm.Reference().Value
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-		err = richClient.StartVM(vmRef)
-		Expect(err).To(BeNil())
-	})
-
-	It("should not throw an error when trying to power off an VM that's already off", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
-		Expect(err).To(BeNil())
-		vmRef := vm.Reference().Value
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-	})
-
-	It("should not throw an error when trying to power on a VM that's already on", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
-		Expect(err).To(BeNil())
-		vmRef := vm.Reference().Value
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-		err = richClient.StartVM(vmRef)
-		Expect(err).To(BeNil())
-		err = richClient.StartVM(vmRef)
-		Expect(err).To(BeNil())
-	})
-})
-
-var _ = Describe("Test VMware rich client with ESXi", func() {
-	var (
-		model               *simulator.Model
-		server              *simulator.Server
-		vm                  *simulator.VirtualMachine
-		username            string
-		password            string
-		simulatorThumbprint = ""
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
 	)
 
-	BeforeEach(func() {
-		model = simulator.ESX()
+	DescribeTable("should retrieve a VM by name", func(model *simulator.Model) {
 		_ = model.Create()
-		server = model.Service.NewServer()
-		vm = simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
-		username = server.URL.User.Username()
-		password, _ = server.URL.User.Password()
-	})
-
-	AfterEach(func() {
-		model.Remove()
-		server.Close()
-	})
-
-	It("should connect to the ESXi host", func() {
-		_, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		vm := simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
 		Expect(err).To(BeNil())
-	})
 
-	It("should retrieve a VM by ID", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
-		Expect(err).To(BeNil())
-		vmRef := vm.Reference().Value
-		rawVm, err := richClient.GetVM(&vmRef, nil, nil, nil)
+		rawVm, err := richClient.GetVM(nil, &vm.Name, nil, nil)
+
 		Expect(err).To(BeNil())
 		retrievedVm, ok := rawVm.(*object.VirtualMachine)
 		Expect(ok).To(BeTrue())
 		Expect(retrievedVm.Reference()).To(Equal(vm.Reference()))
-	})
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
+	)
 
-	It("should power off and on a VM by ID", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
+	DescribeTable("should power off and on a VM by ID", func(model *simulator.Model) {
+		_ = model.Create()
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		vm := simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
 		Expect(err).To(BeNil())
 		vmRef := vm.Reference().Value
+
 		err = richClient.StopVM(vmRef)
 		Expect(err).To(BeNil())
+
 		err = richClient.StartVM(vmRef)
 		Expect(err).To(BeNil())
-	})
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
+	)
 
-	It("should not throw an error when trying to power off an VM that's already off", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
+	DescribeTable("should not throw an error when trying to power off an VM that's already off", func(model *simulator.Model) {
+		_ = model.Create()
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		vm := simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
 		Expect(err).To(BeNil())
 		vmRef := vm.Reference().Value
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
-	})
 
-	It("should not throw an error when trying to power on a VM that's already on", func() {
-		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, simulatorThumbprint)
+		err = richClient.StopVM(vmRef)
+		Expect(err).To(BeNil())
+
+		err = richClient.StopVM(vmRef)
+		Expect(err).To(BeNil())
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
+	)
+
+	DescribeTable("should not throw an error when trying to power on a VM that's already on", func(model *simulator.Model) {
+		_ = model.Create()
+		server := model.Service.NewServer()
+		defer model.Remove()
+		defer server.Close()
+		vm := simulator.Map.Any("VirtualMachine").(*simulator.VirtualMachine)
+		username := server.URL.User.Username()
+		password, _ := server.URL.User.Password()
+		richClient, err := client.NewRichVMWareClient(server.URL.String(), username, password, "")
 		Expect(err).To(BeNil())
 		vmRef := vm.Reference().Value
-		err = richClient.StopVM(vmRef)
-		Expect(err).To(BeNil())
+
 		err = richClient.StartVM(vmRef)
 		Expect(err).To(BeNil())
+
 		err = richClient.StartVM(vmRef)
 		Expect(err).To(BeNil())
-	})
+	},
+		Entry("vCenter", simulator.VPX()),
+		Entry("ESXi", simulator.ESX()),
+	)
 })
