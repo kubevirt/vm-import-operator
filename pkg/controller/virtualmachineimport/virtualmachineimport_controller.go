4024606e602e200b8f0ee8b9ca64522e7a01e16b
Sam Lucidi <slucidi@redhat.com>
2020-07-21 12:13:22 -0400

    Add support for the VMware provider in the import controller
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/controller/virtualmachineimport/virtualmachineimport_controller.go
+++ pkg/controller/virtualmachineimport/virtualmachineimport_controller.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	liberrors "errors"
 	"fmt"
+	"github.com/kubevirt/vm-import-operator/pkg/providers/vmware"
 	"os"
 	"strconv"
 	"strings"
@@ -832,17 +833,22 @@ func isIncomplete(condition *v2vv1.VirtualMachineImportCondition) bool {
 }
 
 func (r *ReconcileVirtualMachineImport) createProvider(vmi *v2vv1.VirtualMachineImport) (provider.Provider, error) {
+	config, err := r.ctrlConfigProvider.GetConfig()
+	if err != nil {
+		log.Error(err, "Cannot get controller config.")
+	}
+
 	// The type of the provider is evaluated based on the source field from the CR
 	if vmi.Spec.Source.Ovirt != nil {
-		config, err := r.ctrlConfigProvider.GetConfig()
-		if err != nil {
-			log.Error(err, "Cannot get controller config.")
-		}
 		provider := ovirtprovider.NewOvirtProvider(vmi.ObjectMeta, vmi.TypeMeta, r.client, r.ocClient, r.factory, r.kvConfigProvider, config)
 		return &provider, nil
 	}
+	if vmi.Spec.Source.Vmware != nil {
+		provider := vmware.NewVmwareProvider(vmi.ObjectMeta, vmi.TypeMeta, r.client, r.ocClient, r.factory, r.kvConfigProvider, config)
+		return &provider, nil
+	}
 
-	return nil, fmt.Errorf("Invalid source type. only Ovirt type is supported")
+	return nil, fmt.Errorf("Invalid source type. Only Ovirt and Vmware type is supported")
 }
 
 func (r *ReconcileVirtualMachineImport) updateToRunning(vmName types.NamespacedName) error {
