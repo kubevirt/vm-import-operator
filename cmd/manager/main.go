6cbece192c982cebc5f56343050cc3bd833c3f5f
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2020-12-20 12:13:00 +0200

    sigs.k8s.io/controller-runtime/pkg/runtime/* packages are deprecated, and were moved to new pathes.
    Trying to upgrade sigs.k8s.io/controller-runtime to version v0.7.0 in HCO created a conflict because in v0.7.0 the deprecated packages were removed and cannot be used.
    
    This PR replaces the deprecated packages with their new pathes.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -35,7 +35,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
 // Change below variables to serve metrics on different host or port.
